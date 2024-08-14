ed4062bb5b113c0f23ad5f366be2390b25265f56
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-14 11:07:25 -0400

    Create Reviews and migrations

--- db/migrate/20240814133920_create_reservations.rb
+++ db/migrate/20240814133920_create_reservations.rb
@@ -1,8 +1,8 @@
 class CreateReservations < ActiveRecord::Migration[5.0]
   def change
     create_table :reservations do |t|
-      t.string :checkin
-      t.string :checkout
+      t.date :checkin
+      t.date :checkout
       t.references :guest, foreign_key: true
       t.references :listing, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20240814125625) do
+ActiveRecord::Schema.define(version: 20240814134453) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "address"
+    t.string   "listing_type"
+    t.integer  "price"
+    t.integer  "neighborhood_id"
+    t.integer  "host_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["host_id"], name: "index_listings_on_host_id"
+    t.index ["neighborhood_id"], name: "index_listings_on_neighborhood_id"
   end
 
   create_table "neighborhoods", force: :cascade do |t|
@@ -24,6 +38,34 @@ ActiveRecord::Schema.define(version: 20240814125625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_neighborhoods_on_city_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "checkin"
+    t.date     "checkout"
+    t.integer  "guest_id"
+    t.integer  "listing_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["guest_id"], name: "index_reservations_on_guest_id"
+    t.index ["listing_id"], name: "index_reservations_on_listing_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "description"
+    t.integer  "rating"
+    t.integer  "guest_id"
+    t.integer  "reservation_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["guest_id"], name: "index_reviews_on_guest_id"
+    t.index ["reservation_id"], name: "index_reviews_on_reservation_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
